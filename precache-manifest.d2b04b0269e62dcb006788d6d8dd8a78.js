17c2a1bd799e8bd0e70bf2404800612e46524bd6
Frithful <casteful@gmail.com>
2019-01-04 12:48:26 +0300

    Updates

---
+++ precache-manifest.d2b04b0269e62dcb006788d6d8dd8a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4176e75f35b6f287c4c9",
+    "url": "/hello/static/css/main.fea13fb5.chunk.css"
+  },
+  {
+    "revision": "4176e75f35b6f287c4c9",
+    "url": "/hello/static/js/main.4176e75f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/hello/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee5f271ac006019c0e9232029cc69297",
+    "url": "/hello/index.html"
+  }
+];
